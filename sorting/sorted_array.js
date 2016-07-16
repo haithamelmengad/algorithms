286af2c37623ee0aa6e3708ebd04904e6fc3d3b8
Mustafa Paksoy <paksoy@gmail.com>
2016-07-16 02:51:00 -0400

    Add sorting assignments.

--- sorting/sorted_array.js
+++ sorting/sorted_array.js
@@ -45,4 +45,65 @@ sortedArray.indexOf = function(sortedArray, item) {
   // YOUR CODE HERE
 }
 
+// Write a function that takes a sorted array and returns a new
+// array with all the duplicate items removed.
+//
+// In a sorted array duplicate items will be adjacent, you can
+// use this to your advantage to make a single run through the data
+// detecting sequences of identical items.
+//
+// ex. sortedArray.removeDuplicates([]) -> []
+// ex. sortedArray.removeDuplicates([1]) -> [1]
+// ex. sortedArray.removeDuplicates([1, 1, 1, 1]) -> [1]
+// ex. sortedArray.removeDuplicates([0, 1, 1, 1, 1, 4, 5]) -> [0, 1, 4, 5]
+// ex. sortedArray.removeDuplicates([-1, 0, 1, 1, 3, 5, 5, 5, 12, 13, 13]) -> [-1, 0, 1, 3, 5, 12, 13]
+sortedArray.removeDuplicates = function(sortedArray) {
+  // YOUR CODE HERE
+}
+
+// Write a function that takes an array and returns a new array with items
+// sorted in increasing order.
+//
+// Use the Bubble Sort algorithm to solve this problem.
+//
+// ex. sortedArray.bubbleSort([]) -> []
+// ex. sortedArray.bubbleSort([1,2,3]) -> [1,2,3]
+// ex. sortedArray.bubbleSort([2,2,2]) -> [2,2,2]
+// ex. sortedArray.bubbleSort([3,2,1,2,4]) -> [1,2,2,3,4]
+// ex. sortedArray.bubbleSort([3, 2, 1]) -> [1, 2, 3]
+// ex. sortedArray.bubbleSort([3, 2, 2, 1]) -> [1, 2, 2, 3]
+sortedArray.bubbleSort = function(array) {
+  // YOUR CODE HERE
+}
+
+// Write a function that takes an array and returns a new array with items
+// sorted in increasing order.
+//
+// Use the merge Sort algorithm to solve this problem.
+//
+// ex. sortedArray.mergeSort([]) -> []
+// ex. sortedArray.mergeSort([1,2,3]) -> [1,2,3]
+// ex. sortedArray.mergeSort([2,2,2]) -> [2,2,2]
+// ex. sortedArray.mergeSort([3,2,1,2,4]) -> [1,2,2,3,4]
+// ex. sortedArray.mergeSort([3, 2, 1]) -> [1, 2, 3]
+// ex. sortedArray.mergeSort([3, 2, 2, 1]) -> [1, 2, 2, 3]
+sortedArray.mergeSort = function(array) {
+  // YOUR CODE HERE
+}
+
+// Write a function that takes an array and returns a new array with items
+// sorted in increasing order.
+//
+// Use the Quick Sort algorithm to solve this problem.
+//
+// ex. sortedArray.quickSort([]) -> []
+// ex. sortedArray.quickSort([1,2,3]) -> [1,2,3]
+// ex. sortedArray.quickSort([2,2,2]) -> [2,2,2]
+// ex. sortedArray.quickSort([3,2,1,2,4]) -> [1,2,2,3,4]
+// ex. sortedArray.quickSort([3, 2, 1]) -> [1, 2, 3]
+// ex. sortedArray.quickSort([3, 2, 2, 1]) -> [1, 2, 2, 3]
+sortedArray.quickSort = function(array) {
+  // YOUR CODE HERE
+}
+
 window.sortedArray = sortedArray;
--- sorting/sorted_array_tests.js
+++ sorting/sorted_array_tests.js
@@ -65,3 +65,96 @@ describe("sortedArray.mergeArrays()", function() {
     expect(sortedArray.mergeArrays([1, 2, 9, 10, 12], [2, 3, 11]) ).toEqual([1, 2, 2, 3, 9, 10, 11, 12]);
   });
 });
+
+describe("sortedArray.removeDuplicates()", function() {
+  it("sortedArray.removeDuplicates([]) -> []", function() {
+    expect(sortedArray.removeDuplicates([]) ).toEqual([]);
+  });
+  it("sortedArray.removeDuplicates([1]) -> [1]", function() {
+    expect(sortedArray.removeDuplicates([1]) ).toEqual([1]);
+  });
+  it("sortedArray.removeDuplicates([1, 1, 1, 1]) -> [1]", function() {
+    expect(sortedArray.removeDuplicates([1, 1, 1, 1]) ).toEqual([1]);
+  });
+  it("sortedArray.removeDuplicates([0, 1, 1, 1, 1, 4, 5]) -> [0, 1, 4, 5]", function() {
+    expect(sortedArray.removeDuplicates([0, 1, 1, 1, 1, 4, 5]) ).toEqual([0, 1, 4, 5]);
+  });
+  it("sortedArray.removeDuplicates([-1, 0, 1, 1, 3, 5, 5, 5, 12, 13, 13]) -> [-1, 0, 1, 3, 5, 12, 13]", function() {
+    expect(sortedArray.removeDuplicates([-1, 0, 1, 1, 3, 5, 5, 5, 12, 13, 13]) ).toEqual([-1, 0, 1, 3, 5, 12, 13]);
+  });
+});
+
+describe("sortedArray.bubbleSort()", function() {
+  it("sortedArray.bubbleSort([]) -> []", function() {
+    expect(sortedArray.bubbleSort([]) ).toEqual([]);
+  });
+  it("sortedArray.bubbleSort([1,2,3]) -> [1,2,3]", function() {
+    expect(sortedArray.bubbleSort([1,2,3]) ).toEqual([1,2,3]);
+  });
+  it("sortedArray.bubbleSort([2,2,2]) -> [2,2,2]", function() {
+    expect(sortedArray.bubbleSort([2,2,2]) ).toEqual([2,2,2]);
+  });
+  it("sortedArray.bubbleSort([3,2,1,2,4]) -> [1,2,2,3,4]", function() {
+    expect(sortedArray.bubbleSort([3,2,1,2,4]) ).toEqual([1,2,2,3,4]);
+  });
+  it("sortedArray.bubbleSort([3, 2, 1]) -> [1, 2, 3]", function() {
+    expect(sortedArray.bubbleSort([3, 2, 1]) ).toEqual([1, 2, 3]);
+  });
+  it("sortedArray.bubbleSort([3, 2, 2, 1]) -> [1, 2, 2, 3]", function() {
+    expect(sortedArray.bubbleSort([3, 2, 2, 1]) ).toEqual([1, 2, 2, 3]);
+  });
+  it("sortedArray.bubbleSort() works on randomly shuffed 10000 item array", function() {
+    var input = _.shuffle(_.range(10000));
+    expect(sortedArray.bubbleSort(input)).toEqual(_.range(10000));
+  });
+});
+
+describe("sortedArray.mergeSort()", function() {
+  it("sortedArray.mergeSort([]) -> []", function() {
+    expect(sortedArray.mergeSort([]) ).toEqual([]);
+  });
+  it("sortedArray.mergeSort([1,2,3]) -> [1,2,3]", function() {
+    expect(sortedArray.mergeSort([1,2,3]) ).toEqual([1,2,3]);
+  });
+  it("sortedArray.mergeSort([2,2,2]) -> [2,2,2]", function() {
+    expect(sortedArray.mergeSort([2,2,2]) ).toEqual([2,2,2]);
+  });
+  it("sortedArray.mergeSort([3,2,1,2,4]) -> [1,2,2,3,4]", function() {
+    expect(sortedArray.mergeSort([3,2,1,2,4]) ).toEqual([1,2,2,3,4]);
+  });
+  it("sortedArray.mergeSort([3, 2, 1]) -> [1, 2, 3]", function() {
+    expect(sortedArray.mergeSort([3, 2, 1]) ).toEqual([1, 2, 3]);
+  });
+  it("sortedArray.mergeSort([3, 2, 2, 1]) -> [1, 2, 2, 3]", function() {
+    expect(sortedArray.mergeSort([3, 2, 2, 1]) ).toEqual([1, 2, 2, 3]);
+  });
+  it("sortedArray.mergeSort() works on randomly shuffed 10000 item array", function() {
+    var input = _.shuffle(_.range(10000));
+    expect(sortedArray.mergeSort(input)).toEqual(_.range(10000));
+  });
+});
+
+describe("sortedArray.quickSort()", function() {
+  it("sortedArray.quickSort([]) -> []", function() {
+    expect(sortedArray.quickSort([]) ).toEqual([]);
+  });
+  it("sortedArray.quickSort([1,2,3]) -> [1,2,3]", function() {
+    expect(sortedArray.quickSort([1,2,3]) ).toEqual([1,2,3]);
+  });
+  it("sortedArray.quickSort([2,2,2]) -> [2,2,2]", function() {
+    expect(sortedArray.quickSort([2,2,2]) ).toEqual([2,2,2]);
+  });
+  it("sortedArray.quickSort([3,2,1,2,4]) -> [1,2,2,3,4]", function() {
+    expect(sortedArray.quickSort([3,2,1,2,4]) ).toEqual([1,2,2,3,4]);
+  });
+  it("sortedArray.quickSort([3, 2, 1]) -> [1, 2, 3]", function() {
+    expect(sortedArray.quickSort([3, 2, 1]) ).toEqual([1, 2, 3]);
+  });
+  it("sortedArray.quickSort([3, 2, 2, 1]) -> [1, 2, 2, 3]", function() {
+    expect(sortedArray.quickSort([3, 2, 2, 1]) ).toEqual([1, 2, 2, 3]);
+  });
+  it("sortedArray.quickSort() works on randomly shuffed 10000 item array", function() {
+    var input = _.shuffle(_.range(10000));
+    expect(sortedArray.quickSort(input)).toEqual(_.range(10000));
+  });
+});
