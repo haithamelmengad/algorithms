0ee1817bc7e77068cd3abde0268769031bf9e160
Mustafa Paksoy <paksoy@gmail.com>
2016-07-16 14:42:17 -0400

    Add radix sort bonus.

--- sorting/sort_bonus.js
+++ sorting/sort_bonus.js
@@ -1,12 +1,28 @@
 "use strict";
 
-// Adapted from Cracking the Coding Interview
 var sortBonus = {};
 
-// Write a function that takes an array of strings and sorts the array such that 
+// Write a function that takes an array and returns a new array with items
+// sorted in increasing order.
+//
+// Use the Quick Sort algorithm to solve this problem.
+//
+// ex. sortBonus.radixSort([]) -> []
+// ex. sortBonus.radixSort([1,2,3]) -> [1,2,3]
+// ex. sortBonus.radixSort([2,2,2]) -> [2,2,2]
+// ex. sortBonus.radixSort([3,2,1,2,4]) -> [1,2,2,3,4]
+// ex. sortBonus.radixSort([3, 2, 1]) -> [1, 2, 3]
+// ex. sortBonus.radixSort([3, 2, 2, 1]) -> [1, 2, 2, 3]
+sortBonus.radixSort = function(array) {
+  // YOUR CODE HERE
+}
+
+// Write a function that takes an array of strings and sorts the array such that
 // strings that are anagrams (i.e. contain the same letters) are grouped together.
 //
 // Use Array.sort() and pass in a custom comparator.
+//
+// Adapted from Cracking the Coding Interview
 //
 // ex. sortBonus.groupAnagrams([]) -> []
 // ex. sortBonus.groupAnagrams(['a', 'b', 'a']) -> ['a', 'a', 'b']
--- sorting/sort_bonus_tests.js
+++ sorting/sort_bonus_tests.js
@@ -1,5 +1,30 @@
 "use strict";
 
+describe("sortBonus.radixSort()", function() {
+  it("sortBonus.radixSort([]) -> []", function() {
+    expect(sortBonus.radixSort([]) ).toEqual([]);
+  });
+  it("sortBonus.radixSort([1,2,3]) -> [1,2,3]", function() {
+    expect(sortBonus.radixSort([1,2,3]) ).toEqual([1,2,3]);
+  });
+  it("sortBonus.radixSort([2,2,2]) -> [2,2,2]", function() {
+    expect(sortBonus.radixSort([2,2,2]) ).toEqual([2,2,2]);
+  });
+  it("sortBonus.radixSort([3,2,1,2,4]) -> [1,2,2,3,4]", function() {
+    expect(sortBonus.radixSort([3,2,1,2,4]) ).toEqual([1,2,2,3,4]);
+  });
+  it("sortBonus.radixSort([3, 2, 1]) -> [1, 2, 3]", function() {
+    expect(sortBonus.radixSort([3, 2, 1]) ).toEqual([1, 2, 3]);
+  });
+  it("sortBonus.radixSort([3, 2, 2, 1]) -> [1, 2, 2, 3]", function() {
+    expect(sortBonus.radixSort([3, 2, 2, 1]) ).toEqual([1, 2, 2, 3]);
+  });
+  it("sortBonus.radixSort() works on randomly shuffed 10000 item array", function() {
+    var input = _.shuffle(_.range(10000));
+    expect(sortBonus.radixSort(input)).toEqual(_.range(10000));
+  });
+});
+
 function normalize(str) {
 	var letters = str.split('');
 	letters.sort();
