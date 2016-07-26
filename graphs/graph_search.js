a30c28e736dee7786a0d117f934cac8aea859b2e
Lane Rettig <lane@joinhorizons.com>
2016-07-26 16:42:49 -0400

    adds graph search exercise

--- graphs/graph_search.js
+++ graphs/graph_search.js
@@ -1,30 +1,41 @@
 "use strict";
 
-// THE GRAPH
+// GRAPH SEARCH
 
-// A graph is one of the most fundamental, important, and broadly useful data
-// structures in computer science. Let's begin by implementing a graph.
+// Let's extend the Graph class we created in the first exercise with a few
+// search-related methods. Note that this code DOES depend upon your code in
+// graph.js from the first exercise.
 
-// As you saw in class, you can implement a graph in multiple ways including
-// by using an adjacency list or an adjacency matrix. Pick one, and fill in the
-// empty methods below to fully implement a graph. For now we'll build an
-// unweighted, undirected graph.
+// BFS and DFS require that the neighbors of each node be sorted. For the
+// purposes of this exercise, sort the neighbors in alphabetical order, e.g.,
+// node "B" comes after (to the right of) "A" and before (to the left of) "C".
 
-// Suggested reading: https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs
-
-// Return true if the graph is connected, false otherwise.
+// Write a function that returns true if the graph is connected, false
+// otherwise. Recall that a graph is connected if every vertex is reachable from
+// every other vertex, i.e., there is no vertex (or set of vertices) that is
+// totally disconnected from the other vertices.
 Graph.prototype.isConnected = function() {
   // YOUR CODE HERE
 };
 
-Graph.prototype.hasCycle = function() {
-
+// Write a function that, given a start vertex, returns the list of vertices
+// visited in order with a depth first search.
+//
+// For instance, starting at A, we'd visit A -> B -> D -> C.
+//
+//     A
+//    / \
+//   B   C
+//  /
+// D
+Graph.prototype.dfs = function(start) {
+  // YOUR CODE HERE
 };
 
-Graph.prototype.dfs = function() {
-
-};
-
-Graph.prototype.bfs = function() {
-
+// Write a function that, given a start vertex, returns the list of vertices
+// visited in order with a breadth first search.
+//
+// For instance, for the above graph, we'd visit A -> B -> C -> D.
+Graph.prototype.bfs = function(start) {
+  // YOUR CODE HERE
 };
--- graphs/graph_search_tests.js
+++ graphs/graph_search_tests.js
@@ -1,26 +1,119 @@
 "use strict";
 
-describe("graph", function() {
-  var graph;
-  beforeEach(function() {
-    graph = new Graph();
-    graph.addVertex("A");
-    graph.addVertex("B");
-    graph.addVertex("C");
-    graph.addVertex("D");
-    graph.addVertex("E");
-    graph.addEdge("A","B");
-    graph.addEdge("B","C");
-    graph.addEdge("B","D");
-    graph.addEdge("B","E");
-    graph.addEdge("A","E");
-  });
-  describe("an empty graph", function() {
-    it("has no vertices", function() {
+describe("graph search", function() {
+  describe("isConnected", function() {
+    it("returns true for a single-vertex graph", function() {
       var g = new Graph();
-      expect(g.vertices().toEqual(jasmine.any(Array)));
-      expect(g.vertices().length.toBe(0));
+      g.addVertex("A");
+      expect(g.isConnected()).toBeTruthy();
     });
-    
-  })
+    it("returns true for a small connected graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addEdge("A","B");
+      expect(g.isConnected()).toBeTruthy();
+    });
+    it("returns false for a small unconnected graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      expect(g.isConnected()).toBeFalsy();
+    });
+    it("returns true for a large connected graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+      g.addEdge("A","D");
+      g.addEdge("D","E");
+      expect(g.isConnected()).toBeTruthy();
+    });
+    it("returns false for a large unconnected graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addEdge("A","B");
+      g.addEdge("A","D");
+      g.addEdge("D","E");
+      expect(g.isConnected()).toBeFalsy();
+    });
+  });
+  describe("depth-first search", function() {
+    it("returns sole vertex for a single-vertex graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      expect(g.dfs()).toEqual(["A"]);
+    });
+    it("follows a chain of vertices", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+      g.addEdge("C","D");
+      g.addEdge("D","E");
+      expect(g.dfs()).toEqual(["A","B","C","D","E"]);
+    });
+    it("goes deep not broad", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addVertex("F");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+      g.addEdge("B","F");
+      g.addEdge("C","D");
+      g.addEdge("D","E");
+      expect(g.dfs()).toEqual(["A","B","C","D","E","F"]);
+    });
+  });
+  describe("breadth-first search", function() {
+    it("returns sole vertex for a single-vertex graph", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      expect(g.bfs()).toEqual(["A"]);
+    });
+    it("follows a chain of vertices", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+      g.addEdge("C","D");
+      g.addEdge("D","E");
+      expect(g.bfs()).toEqual(["A","B","C","D","E"]);
+    });
+    it("goes broad not deep", function() {
+      var g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addVertex("D");
+      g.addVertex("E");
+      g.addVertex("F");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+      g.addEdge("B","F");
+      g.addEdge("C","D");
+      g.addEdge("D","E");
+      expect(g.bfs()).toEqual(["A","B","C","F","D","E"]);
+    });
+  });
 });
