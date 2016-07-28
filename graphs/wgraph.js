23387f16df9d7fd96e84f39a92ced3f0fd0145e7
Lane Rettig <lane@joinhorizons.com>
2016-07-28 15:39:52 -0400

    wgraph should start from scratch rather than extending; add graph tests to wgraph tests

--- graphs/wgraph.js
+++ graphs/wgraph.js
@@ -2,33 +2,59 @@
 
 // WEIGHTED GRAPH
 
-// Note: This code depends on your code in graph.js from the first exercise.
-
 // A weighted graph contains a weight for every edge. This "weight" is an
 // abstract value and could represent various "real world" data such as distance
 // (between two points or cities), time (to complete a task), money, etc.
 
-// Extend your (unweighted) Graph class from before by adding weights to each
-// edge. Rewrite the methods below (which will overwrite the old ones in the
-// Graph class), and fill in the new method, getWeight.
+// Rewrite the Graph class to be a weighted graph. You'll probably want to copy
+// and paste a lot of your code from graph.js. There is only one brand new
+// method below, getWeight(), but you will have to modify several other methods.
 
 // Constructor
-var oldPrototype = Graph.prototype;
-Graph = function() {
+var WGraph = function() {
   // YOUR CODE HERE
-  // (If you don't need to modify your constructor, just comment out this
-  // method.)
 };
-Graph.prototype = oldPrototype;
 
-// Add an weighted edge linking vertices v1 and v2.
-Graph.prototype.addEdge = function(v1, v2, weight) {
+// Add a vertex called name to the graph.
+WGraph.prototype.addVertex = function(name) {
+  // YOUR CODE HERE
+};
+
+// Add an edge linking vertices v1 and v2.
+WGraph.prototype.addEdge = function(v1, v2, weight) {
+  // YOUR CODE HERE
+};
+
+// Return the list of vertices.
+WGraph.prototype.vertices = function() {
+  // YOUR CODE HERE
+};
+
+// Remove the vertex called name from the graph.
+WGraph.prototype.removeVertex = function(name) {
+  // YOUR CODE HERE
+};
+
+// Remove the edge linking vertices v1 and v2.
+WGraph.prototype.removeEdge = function(v1, v2) {
+  // YOUR CODE HERE
+};
+
+// Return true if vertices v1 and v2 are adjacent, false otherwise.
+WGraph.prototype.isAdjacent = function(v1, v2) {
+  // YOUR CODE HERE
+};
+
+// Return the list of neighbors of the vertex v.
+WGraph.prototype.neighbors = function(v) {
   // YOUR CODE HERE
 };
 
 // New function: return the weight of the edge between v1 and v2. Note: this
 // method should only return a weight if there is a _direct_ edge between v1
 // and v2.
-Graph.prototype.getWeight = function(v1, v2) {
+WGraph.prototype.getWeight = function(v1, v2) {
   // YOUR CODE HERE
 };
+
+window.WGraph = WGraph;
--- graphs/wgraph_tests.js
+++ graphs/wgraph_tests.js
@@ -1,12 +1,154 @@
 "use strict";
 
 describe("weighted graph", function() {
-  it("can set and get edge weight", function() {
-    var g = new Graph();
-    g.addVertex("A");
-    g.addVertex("B");
-    g.addEdge("A","B",3);
-    expect(g.getWeight("A","B")).toBe(3);
-    expect(g.getWeight("B","A")).toBe(3);
+  describe("an empty graph", function() {
+    it("has no vertices", function() {
+      var g = new Graph();
+      expect(g.vertices()).toEqual(jasmine.any(Array));
+      expect(g.vertices().length).toBe(0);
+    });
+  });
+  describe("a single-vertex graph", function() {
+    var g;
+    beforeEach(function() {
+      g = new Graph();
+      g.addVertex("A");
+    });
+    it("has one vertex", function() {
+      expect(g.vertices()).toEqual(jasmine.any(Array));
+      expect(g.vertices().length).toBe(1);
+    });
+    it("has one vertex called A", function() {
+      expect(g.vertices()).toEqual(["A"]);
+    });
+    it("has no neighbors", function() {
+      expect(g.neighbors("A")).toEqual(jasmine.any(Array));
+      expect(g.neighbors("A")).toEqual([]);
+    });
+  });
+  describe("a single-edge graph", function() {
+    var g;
+    beforeEach(function() {
+      g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addEdge("A","B");
+    });
+    it("has two vertices", function() {
+      expect(g.vertices()).toEqual(jasmine.any(Array));
+      expect(g.vertices().length).toBe(2);
+    });
+    it("has vertices called A and B", function() {
+      expect(g.vertices()).toEqual(jasmine.arrayContaining(["A","B"]));
+    });
+    it("isAdjacent('A','B') -> true", function() {
+      expect(g.isAdjacent("A","B")).toBeTruthy();
+    });
+    it("isAdjacent('B','A') -> true", function() {
+      expect(g.isAdjacent("B","A")).toBeTruthy();
+    });
+    it("neighbors('A') -> ['B']", function() {
+      expect(g.neighbors("A")).toEqual(jasmine.any(Array));
+      expect(g.neighbors("A")).toEqual(["B"]);
+    });
+    it("neighbors('B') -> ['A']", function() {
+      expect(g.neighbors("B")).toEqual(jasmine.any(Array));
+      expect(g.neighbors("B")).toEqual(["A"]);
+    });
+  });
+  describe("a two-edge graph", function() {
+    var g;
+    beforeEach(function() {
+      g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addEdge("A","B");
+      g.addEdge("B","C");
+    });
+    it("isAdjacent('A','B') -> true", function() {
+      expect(g.isAdjacent("A","B")).toBe(true);
+    });
+    it("isAdjacent('B','C') -> true", function() {
+      expect(g.isAdjacent("B","C")).toBeTruthy();
+    });
+    it("isAdjacent('A','C') -> false", function() {
+      expect(g.isAdjacent("A","C")).toBeFalsy();
+    });
+    it("isAdjacent('C','A') -> false", function() {
+      expect(g.isAdjacent("C","A")).toBeFalsy();
+    });
+    it("neighbors('A') -> ['B']", function() {
+      expect(g.neighbors("A")).toEqual(["B"]);
+    });
+    it("neighbors('B') -> ['A','C']", function() {
+      expect(g.neighbors("B").length).toBe(2);
+      expect(g.neighbors("B")).toEqual(jasmine.arrayContaining(["A","C"]));
+    });
+    it("neighbors('C') -> ['B']", function() {
+      expect(g.neighbors("C")).toEqual(["B"]);
+    });
+  });
+  describe("adding and removing an edge", function() {
+    var g;
+    beforeEach(function() {
+      g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addEdge("A","B");
+      g.addEdge("A","C");
+      g.addEdge("B","C");
+    });
+    it("removeEdge('A','C') -> isAdjacent('A','C') -> false", function() {
+      g.removeEdge("A","C");
+      expect(g.isAdjacent("A","C")).toBeFalsy();
+    });
+    it("removeEdge('C','A') -> isAdjacent('A','C') -> false", function() {
+      g.removeEdge("C","A");
+      expect(g.isAdjacent("A","C")).toBeFalsy();
+    });
+    it("removeEdge('A','C') -> isAdjacent('C','A') -> false", function() {
+      g.removeEdge("A","C");
+      expect(g.isAdjacent("C","A")).toBeFalsy();
+    });
+    it("removeEdge('C','A') -> isAdjacent('C','A') -> false", function() {
+      g.removeEdge("C","A");
+      expect(g.isAdjacent("C","A")).toBeFalsy();
+    });
+    it("removeEdge('C','A') -> neighbors('A') -> ['B']", function() {
+      g.removeEdge("C","A");
+      expect(g.isAdjacent("C","A")).toBeFalsy();
+    });
+  });
+  describe("adding and removing a vertex", function() {
+    var g;
+    beforeEach(function() {
+      g = new Graph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addVertex("C");
+      g.addEdge("A","B");
+      g.addEdge("A","C");
+      g.addEdge("B","C");
+      g.removeVertex("A");
+    });
+    it("removes the vertex", function() {
+      expect(g.vertices().length).toBe(2);
+      expect(g.vertices()).toEqual(jasmine.arrayContaining(["B","C"]));
+    });
+    it("neighbors('B') -> ['C']", function() {
+      expect(g.neighbors("B")).toEqual(["C"]);
+    });
+  });
+  describe("edge weights", function() {
+    it("can set and get edge weight", function() {
+      var g = new WGraph();
+      g.addVertex("A");
+      g.addVertex("B");
+      g.addEdge("A","B",3);
+      expect(g.getWeight("A","B")).toBe(3);
+      expect(g.getWeight("B","A")).toBe(3);
+    });
   });
 });
