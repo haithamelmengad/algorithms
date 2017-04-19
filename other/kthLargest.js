1bb7b8d43adccad295f85268e1e330f6dce31a90
Moose Paksoy <mustafa@joinhorizons.com>
2017-04-19 12:37:07 -0700

    Implement partition

--- other/kthLargest.js
+++ other/kthLargest.js
@@ -20,6 +20,27 @@ function kthLargest(numbers, k) {
   return heap.peek();
 }
 
+var partition = require('./partition');
+
+function kthLargestPartition(numbers, k) {
+  // https://gist.github.com/KenCorbettJr/4691882
+  function helper(left, right) {
+    if (left >= right) {
+      return numbers[left];
+    }
+    var pivot = partition(numbers, left, right);
+    if (pivot === k) {
+      return numbers[k];
+    } else if (pivot < k) {
+      return helper(left, k - 1);
+    } else {
+      return helper(k + 1, right);
+    }
+  }
+
+  return helper(0, numbers.length);
+}
+
 describe("kthLargest()", function() {
   it("kthLargest([5, -6, 2, 14], 0) -> 14", function() {
     expect(kthLargest([5, -6, 2, 14], 0) ).toBe(14);
@@ -29,5 +50,17 @@ describe("kthLargest()", function() {
   });
   it("kthLargest([5, -6, 2, 14], 2) -> 2", function() {
     expect(kthLargest([5, -6, 2, 14], 2) ).toBe(2);
+  });
+});
+
+describe("kthLargestPartition()", function() {
+  it("kthLargestPartition([5, -6, 2, 14], 0) -> 14", function() {
+    expect(kthLargestPartition([5, -6, 2, 14], 0) ).toBe(14);
+  });
+  it("kthLargestPartition([5, -6, 2, 14], 1) -> 5", function() {
+    expect(kthLargestPartition([5, -6, 2, 14], 1) ).toBe(5);
+  });
+  it("kthLargestPartition([5, -6, 2, 14], 2) -> 2", function() {
+    expect(kthLargestPartition([5, -6, 2, 14], 2) ).toBe(2);
   });
 });
